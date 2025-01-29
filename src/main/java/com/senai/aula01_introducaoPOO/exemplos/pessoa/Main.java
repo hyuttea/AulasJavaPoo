1bbd63314f9d38a74de9733839fc42448b441d61
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-01-29 10:14:03 -0300

    poo aula 2

--- src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
@@ -14,6 +14,7 @@ public class Main {
                         "lauzane",
                         "Sao Paulo",
                         "SP"
+
                 )
         );
         System.out.println(pessoa1);
