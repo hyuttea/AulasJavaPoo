7ae919843f96502bf686f410a7c0e3657c155caa
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-01-29 10:02:19 -0300

    poo aula 2

--- src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Main.java
@@ -4,7 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("Rafa",19);
 
-        System.out.println(pessoa1.nome+", "+pessoa1.idade+", "+ pessoa1.altura+", "+pessoa1.endereco);
+        Pessoa pessoa2 = new Pessoa (
+                "juliano",
+                48,
+                1.70f,
+                new Endereco(
+                        "Guaca",
+                        66,
+                        "lauzane",
+                        "Sao Paulo",
+                        "SP"
+                )
+        );
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
 
+        pessoa1.comer("cenoura");
+        pessoa2.comer("bacon");
     }
 }
--- src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Pessoa.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exemplos/pessoa/Pessoa.java
@@ -4,19 +4,29 @@ public class Pessoa {
     String nome;
     int idade;
     float altura;
-    String endereco;
+    Endereco endereco;
 
     public Pessoa(String nome, int idade){
         this.nome = nome;
         this.idade = idade;
     }
-
     public void falar (String frase){
-        System.out.println("A pessoa  falou: "+frase);
+        System.out.println(nome+" falou: "+frase);
+    }
+    public void comer (String alimento){
+        System.out.println(nome+" comeu: "+alimento);
     }
 
-    public void comer (String alimento){
-        System.out.println("A pessoa comeu: "+alimento);
+    public Pessoa (String nome, int idade, float altura, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.endereco = endereco;
+    }
+    @Override //anotation
+    public String toString() {
+        return "O nome da pessoa é "+this.nome+" tem "+this.idade+" anos, "+this.altura+" e mora no endereço "+this.endereco;
+
     }
 }
 
