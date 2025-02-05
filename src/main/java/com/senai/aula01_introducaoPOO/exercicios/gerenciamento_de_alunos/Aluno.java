34214601102a6b01af00d1fd423d5e4505764c1c
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-02-05 09:10:50 -0300

    ex02 att 5/2

--- src/main/java/com/senai/aula01_introducaoPOO/exercicios/gerenciamento_de_alunos/Aluno.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exercicios/gerenciamento_de_alunos/Aluno.java
@@ -14,16 +14,19 @@ public class Aluno {
     }
     public void exibirInformacoes(){
         System.out.println("O aluno se chama "+nome+", tem "+idade+" anos de idade, sua nota foi "+nota);
+
     }
     public void atualizaNota(double notaRecebida){
-       Scanner input = new Scanner(System.in);
-
+       this.nota = notaRecebida;
+        System.out.println("A nota anterior do "+nome+ " foi atualizada para "+nota );
     }
     public void verificaAprovacao(){
         if (nota >= 6.0 ) {
             System.out.println("\tParabéns! você foi aprovado");
+
         }else {
             System.out.println("\tVocê foi reprovado :(");
+
 
         }
     }
--- src/main/java/com/senai/aula01_introducaoPOO/exercicios/gerenciamento_de_alunos/Main.java
+++ src/main/java/com/senai/aula01_introducaoPOO/exercicios/gerenciamento_de_alunos/Main.java
@@ -1,35 +1,80 @@
 package com.senai.aula01_introducaoPOO.exercicios.gerenciamento_de_alunos;
 
+import java.util.Scanner;
+
 public class Main {
+    static Aluno[] alunos = new Aluno[3];
     public static void main(String[] args) {
-    Aluno aluno1 = new Aluno(
-            "Laura",
-            18,
-            5.0
-    );
-    Aluno aluno2 = new Aluno(
-            "oto",
-            23,
-            8.0
+        Scanner scanner = new Scanner(System.in);
+        int opcoes = 0;
+        System.out.println("O que deseja fazer?");
+        System.out.println("\t1- Exibir Informaçoes dos Alunos");
+        System.out.println("\t2- Atualizar Nota");
+        System.out.println("\t3- Verificar Aprovação");
+        System.out.println("\t4- Sair");
 
-    );
-    Aluno aluno3 = new Aluno(
-            "Guilherme",
-            14,
-            6.0
+        switch (opcoes) {
+            case 1:
+            exibirAlunos();
+                break;
+            case 2:
 
-    );
+                break;
+            case 3:
+
+                break;
+            case 4:
+                System.out.println("Você saiu com exito");
+                break;
+        }
+
+        Aluno aluno1 = new Aluno(
+                "Laura",
+                18,
+                5.0
+        );
+        Aluno aluno2 = new Aluno(
+                "oto",
+                23,
+                8.0
+
+        );
+        Aluno aluno3 = new Aluno(
+                "Guilherme",
+                14,
+                6.0
+
+        );
+
+        alunos[0] = aluno1;
+        alunos[1] = aluno2;
+        alunos[2] = aluno3;
+
+
         System.out.println();
         System.out.println();
         System.out.println();
         aluno1.exibirInformacoes();
         aluno1.verificaAprovacao();
+        aluno1.atualizaNota(6.9);
 
         aluno2.exibirInformacoes();
         aluno2.verificaAprovacao();
+        aluno2.atualizaNota(8.5);
 
         aluno3.exibirInformacoes();
         aluno3.verificaAprovacao();
+        aluno3.atualizaNota(2.4);
     }
 
+    public static void exibirAlunos(){
+        for (int i = 0; i <alunos.length ; i++) {
+            alunos[i].exibirInformacoes();
+        }
+    }
+    public static void cadastroDeAlunos(){
+    }
 }
+
+
+
