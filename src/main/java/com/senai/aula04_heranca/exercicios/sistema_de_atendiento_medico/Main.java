065a02e00615385fa2f55a768729320cabc128ec
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-04-11 20:45:12 -0300

    exercicios herança

--- src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_atendiento_medico/Main.java
+++ src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_atendiento_medico/Main.java
@@ -1,4 +1,15 @@
 package com.senai.aula04_heranca.exercicios.sistema_de_atendiento_medico;
 
 public class Main {
+    public static void main(String[] args) {
+    PacienteConvenio pacienteConvenio = new PacienteConvenio("Jorge", 30,50,200);
+    PacienteParticular pacienteParticular = new PacienteParticular("Laura ",19,300);
+
+pacienteConvenio.exbibirInfos();
+pacienteConvenio.descontoConvenio();
+
+pacienteParticular.exbibirInfos();
+pacienteParticular.total();
+
+    }
 }
--- src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_bibliotecas/Main.java
+++ src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_bibliotecas/Main.java
@@ -1,4 +1,14 @@
 package com.senai.aula04_heranca.exercicios.sistema_de_bibliotecas;
 
 public class Main {
+    public static void main(String[] args) {
+        LivroFisico livroFisico = new LivroFisico("Beyond |","Lucio Fulci ",300);
+        LivroDigital livroDigital = new LivroDigital("yyy |","xxx ",20.8);
+
+        livroDigital.exibirInfo();
+        livroDigital.tamanhoArquivo();
+
+        livroFisico.exibirInfo();
+        livroFisico.numeroPaginas();
+    }
 }
