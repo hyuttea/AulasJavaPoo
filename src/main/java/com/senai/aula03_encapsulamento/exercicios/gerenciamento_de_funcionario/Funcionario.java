97a29e7c17ec10b649af4912f13835327e90b3cc
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-04-12 11:03:08 -0300

    exercicio encapsulamento

--- src/main/java/com/senai/aula03_encapsulamento/exercicios/gerenciamento_de_funcionario/Funcionario.java
+++ src/main/java/com/senai/aula03_encapsulamento/exercicios/gerenciamento_de_funcionario/Funcionario.java
@@ -1,4 +1,51 @@
 package com.senai.aula03_encapsulamento.exercicios.gerenciamento_de_funcionario;
 
 public class Funcionario {
+    private String nome;
+    private String cargo;
+    private double salario;
+
+    public Funcionario(String nome, String cargo, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void alterarSalario(double valor) {
+        if (valor > 1320) {
+            salario = valor;
+            System.out.println("Salário de " + nome + " redefinido para R$" + salario);
+        } else {
+            System.out.println("Inválido! Valor menor que um salário minimo");
+        }
+    }
+
+    public void calculoAumento(double aumento) {
+        System.out.println("Salário promovido para R$" + (aumento + salario));
+    }
 }
+
--- src/main/java/com/senai/aula03_encapsulamento/exercicios/gerenciamento_de_funcionario/Main.java
+++ src/main/java/com/senai/aula03_encapsulamento/exercicios/gerenciamento_de_funcionario/Main.java
@@ -2,6 +2,9 @@ package com.senai.aula03_encapsulamento.exercicios.gerenciamento_de_funcionario;
 
 public class Main {
     public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Laura","Professora",1400);
+        funcionario.calculoAumento(120);
+        funcionario.alterarSalario(1800);
 
     }
 }
